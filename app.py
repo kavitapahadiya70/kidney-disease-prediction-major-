9317cec6032d537e63fd0e0a7f6c30edf381d079
Kavita Pahadiya <99033559+kavitapahadiya70@users.noreply.github.com>
2022-08-21 20:00:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,113 +1,66 @@
-from flask import Flask, render_template, request, flash, redirect
-import pickle
+from flask import Flask, request, jsonify, render_template
+from flask_ngrok import run_with_ngrok
 import numpy as np
-from PIL import Image
-from tensorflow.keras.models import load_model
-
+import pickle
 
 
 app = Flask(__name__)
+run_with_ngrok(app)
 
-def predict(values, dic):
-    if len(values) == 8:
-        model = pickle.load(open('models/diabetes.pkl','rb'))
-        values = np.asarray(values)
-        return model.predict(values.reshape(1, -1))[0]
-    elif len(values) == 26:
-        model = pickle.load(open('models/breast_cancer.pkl','rb'))
-        values = np.asarray(values)
-        return model.predict(values.reshape(1, -1))[0]
-    elif len(values) == 13:
-        model = pickle.load(open('models/heart.pkl','rb'))
-        values = np.asarray(values)
-        return model.predict(values.reshape(1, -1))[0]
-    elif len(values) == 18:
-        model = pickle.load(open('models/kidney.pkl','rb'))
-        values = np.asarray(values)
-        return model.predict(values.reshape(1, -1))[0]
-    elif len(values) == 10:
-        model = pickle.load(open('models/liver.pkl','rb'))
-        values = np.asarray(values)
-        return model.predict(values.reshape(1, -1))[0]
+    
+model_RF=pickle.load(open('/content/drive/My Drive/Major_RF.pkl', 'rb')) 
+model_KNN=pickle.load(open('/content/drive/My Drive/Major_KNN.pkl', 'rb')) 
+model_K_SVM=pickle.load(open('/content/drive/My Drive/Major_SVM_linear.pkl', 'rb')) 
+model_DT=pickle.load(open('/content/drive/My Drive/Major_DT.pkl', 'rb')) 
+model_NB=pickle.load(open('/content/drive/My Drive/Major_NB.pkl', 'rb')) 
 
-@app.route("/")
+
+
+@app.route('/')
 def home():
-    return render_template('home.html')
+  
+    return render_template("index.html")
+#------------------------------About us-------------------------------------------
+("name=kavita pahadiya")
+@app.route('/aboutusnew')
+def aboutusnew():
+    return render_template('aboutusnew.html')
+  
+@app.route('/predict',methods=['GET'])
 
-@app.route("/diabetes", methods=['GET', 'POST'])
-def diabetesPage():
-    return render_template('diabetes.html')
+def predict():
+    
+     
+    age = float(request.args.get('age'))
+    sex = float(request.args.get('sex'))
+    chest_pain = float(request.args.get('chest_pain'))
+    bp = float(request.args.get('bp'))
+    st = float(request.args.get('st'))
 
-@app.route("/cancer", methods=['GET', 'POST'])
-def cancerPage():
-    return render_template('breast_cancer.html')
+    Model = (request.args.get('Model'))
 
-@app.route("/heart", methods=['GET', 'POST'])
-def heartPage():
-    return render_template('heart.html')
+    if Model=="Random Forest Classifier":
+      prediction = model_RF.predict([[age, sex, chest_pain, bp, st]])
 
-@app.route("/kidney", methods=['GET', 'POST'])
-def kidneyPage():
-    return render_template('kidney.html')
+    elif Model=="Decision Tree Classifier":
+      prediction = model_DT.predict([[age, sex, chest_pain, bp, st]])
 
-@app.route("/liver", methods=['GET', 'POST'])
-def liverPage():
-    return render_template('liver.html')
+    elif Model=="KNN Classifier":
+      prediction = model_KNN.predict([[age, sex, chest_pain, bp, st]])
 
-@app.route("/malaria", methods=['GET', 'POST'])
-def malariaPage():
-    return render_template('malaria.html')
+    elif Model=="SVM Classifier":
+      prediction = model_K_SVM.predict([[age, sex, chest_pain, bp, st]])
 
-@app.route("/pneumonia", methods=['GET', 'POST'])
-def pneumoniaPage():
-    return render_template('pneumonia.html')
+    else:
+      prediction = model_NB.predict([[age, sex, chest_pain, bp, st]])
 
-@app.route("/predict", methods = ['POST', 'GET'])
-def predictPage():
-    try:
-        if request.method == 'POST':
-            to_predict_dict = request.form.to_dict()
-            to_predict_list = list(map(float, list(to_predict_dict.values())))
-            pred = predict(to_predict_list, to_predict_dict)
-    except:
-        message = "Please enter valid Data"
-        return render_template("home.html", message = message)
+    
+    if prediction == [0]:
+      return render_template('index.html', prediction_text="NO kidney  disease", extra_text =" -- Prediction by " + Model)
 
-    return render_template('predict.html', pred = pred)
+    else :
+      return render_template('index.html', prediction_text='kidney disease', extra_text =" -- Prediction by " + Model)
 
-@app.route("/malariapredict", methods = ['POST', 'GET'])
-def malariapredictPage():
-    if request.method == 'POST':
-        try:
-            if 'image' in request.files:
-                img = Image.open(request.files['image'])
-                img = img.resize((36,36))
-                img = np.asarray(img)
-                img = img.reshape((1,36,36,3))
-                img = img.astype(np.float64)
-                model = load_model("models/malaria.h5")
-                pred = np.argmax(model.predict(img)[0])
-        except:
-            message = "Please upload an Image"
-            return render_template('malaria.html', message = message)
-    return render_template('malaria_predict.html', pred = pred)
-
-@app.route("/pneumoniapredict", methods = ['POST', 'GET'])
-def pneumoniapredictPage():
-    if request.method == 'POST':
-        try:
-            if 'image' in request.files:
-                img = Image.open(request.files['image']).convert('L')
-                img = img.resize((36,36))
-                img = np.asarray(img)
-                img = img.reshape((1,36,36,1))
-                img = img / 255.0
-                model = load_model("models/pneumonia.h5")
-                pred = np.argmax(model.predict(img)[0])
-        except:
-            message = "Please upload an Image"
-            return render_template('pneumonia.html', message = message)
-    return render_template('pneumonia_predict.html', pred = pred)
-
-if __name__ == '__main__':
-	app.run(debug = True)
+app.run()
+# if __name__ == "__main__":
+#     app.run(debug=True)
